08b5641cd32ac8bb36e8936aca61cd2d691be829
KaviyaShree-V <karthikavi1976@gmail.com>
2025-01-18 16:53:54 +0530

    Update BookMyShow.java

--- BookMyShow/BookMyShow.java
+++ BookMyShow/BookMyShow.java
@@ -5,14 +5,26 @@ public class BookMyShow {
     public static ArrayList<Admin> admin= new ArrayList<>();
     public static ArrayList<User> user=new ArrayList<>();
 
+    public static ArrayList<Theatre> theatres = new ArrayList<>();
+    private static HashMap<String , Theatre> theatreName = new HashMap<>();
+
     static {
-        admin.add(new Admin("BMS","bms565"));
+        admin.add(new Admin("BMS", "bms565"));
     }
+
     public static ArrayList<Admin> getAdmin() {
         return admin;
     }
 
     public static ArrayList<User> getUser() {
         return user;
+    }
+
+    public static ArrayList<Theatre> getTheatres() {
+        return theatres;
+    }
+
+    public static HashMap<String,Theatre> getTheatreName() {
+        return theatreName;
     }
 }
