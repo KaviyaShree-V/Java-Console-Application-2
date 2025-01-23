2b1ed2a6f0df864dd27820f6b566dbfc9dcdba81
KaviyaShree-V <karthikavi1976@gmail.com>
2025-01-23 21:51:05 +0530

    Update User.java

--- BookMyShow/User.java
+++ BookMyShow/User.java
@@ -1,10 +1,17 @@
-public class User {
-    String userName;
-    String userPassword;
+import java.util.ArrayList;
 
-    public User(String userName, String userPassword) {
+public class User {
+    private String userName;
+    private String userPassword;
+    private String surName;
+    private String tLocation;
+    private ArrayList<Tickets> tickets =new ArrayList<>();
+
+    public User(String userName, String userPassword, String thName,String tLocation) {
         this.userName=userName;
         this.userPassword=userPassword;
+        this.surName =thName;
+        this.tLocation=tLocation;
     }
 
     public String getUserPassword() {
@@ -13,5 +20,17 @@ public class User {
 
     public String getUserName() {
         return userName;
+    }
+
+    public ArrayList<Tickets> getTickets() {
+        return tickets;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public String gettLocation() {
+        return tLocation;
     }
 }
